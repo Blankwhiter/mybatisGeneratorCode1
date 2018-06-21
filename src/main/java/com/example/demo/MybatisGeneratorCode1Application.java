6091fe27e3bd319c3499b7df06a243920e541780
Blankwhiter <belonghuang@126.com>
2018-06-21 10:55:28 +0800

    通过main方式
    mybatis 通过main方式生成Mapper  dao PO代码

---
+++ src/main/java/com/example/demo/MybatisGeneratorCode1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorCode1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGeneratorCode1Application.class, args);
+	}
+}
